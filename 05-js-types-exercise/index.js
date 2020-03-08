df3eb64b2d0d8eb9ebdb265578348a1dcde80f09
Hodan Abdullahi <Hodan@Hodans-MacBook-Pro.local>
2020-03-08 23:12:28 +0000

    Commit

--- 05-js-types-exercise/index.js
+++ 05-js-types-exercise/index.js
@@ -1,3 +1,20 @@
 const greeting =  "Hello";
 const name = "Mimi";
-const phrase = greeting + ", " + name;
+const phrase = greeting + ", " + name + "!";
+
+const number = 29;
+const price = 12.23;
+const quantity= 32;
+const total = (price * quantity);
+
+const age1 = 1;
+const age2 = 2;
+const age3 = 3;
+const averageAge =((age1 + age2 + age3 )/3);
+
+let loggedIn =  true;
+const adminUser = false;
+const missingInfo= undefined;
+
+
+
