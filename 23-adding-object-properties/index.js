e0ad42cf9d0570742cd8843e92b7013c4043b046
h.abdullahi@outlook.com <h.abdullahi@outlook.com>
2020-03-30 21:57:48 +0100

    commit

--- 23-adding-object-properties/index.js
+++ 23-adding-object-properties/index.js
@@ -10,15 +10,35 @@ const user = {};
  * add property "firstName" to "user" and assign any name 
  */
 
+
+
+
+user.firstName = "Hodan";
+
 /**
  * Exercise 2
  * 
  * add property "lastName" to "user" and assign any last name 
  */
 
-/**
+ user.lastName = "Abdullahi"
+ 
+ /**
  * Exercise 3
  * create a function "addANewProperty" which takes
  * 2 params, first - property name and second - property value
  * and set a new property for 'user' object
  */
+
+
+
+ function addANewProperty() {
+     Object.defineProperty(user, "age", {
+     value: 25
+   
+
+     }); 
+
+return user.age
+
+    }
